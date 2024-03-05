f934199f67e62f4a13f9af5add4c75cb7ad3f20a
Radoinov <radoinov08@gmail.com>
2024-03-05 20:59:57 +0200

    Homework

---
+++ SortingNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SortingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int swap = nums[i];
+                int index = i;
+                while (index > 0 && nums[index - 1] >= swap)
+                {
+                    nums[index] = nums[index - 1];
+                    index--;
+                }
+                nums[index] = swap;
+            }
+            Console.WriteLine(string.Join(" <= ", nums));
+        }
+    }
+}
---
+++ SquaredSorting/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SquaredSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> squares = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == Math.Sqrt(nums[i]) * Math.Sqrt(nums[i]))
+                {
+                    squares.Add(nums[i]);
+                }
+            }
+            squares.Sort((a, b) => b.CompareTo(a));
+            Console.WriteLine(string.Join(" ", squares));
+        }
+    }
+}
